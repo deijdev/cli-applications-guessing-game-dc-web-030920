74bb55f0ac7a13b2a915a79a025a33e9f1d5c6b4
Deijah Price <deijahwilson1@gmail.com>
2020-03-06 04:26:42 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -11,7 +11,7 @@ def run_guessing_game
       elsif guess.to_i == computer_number
           puts "You guessed the correct number!"
         elsif guess.to_i != computer_number
-          puts "The computer guessed #{computer_number}."
+          puts "Sorry! The computer guessed #{computer_number}."
         else
           puts "Invalid input"
         end
